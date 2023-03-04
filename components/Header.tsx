c8007869e652ee33da3865ceed6b97449fac05ad
Hanif Carroll <hcarroll@leagueapps.com>
2023-03-04 13:57:56 -0300

    Add z-index to header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function Header({ language }: { language?: string }) {
   return (
-    <header className="sticky top-0 mb-10 p-3 border-2 border-gray-800 w-full bg-white flex justify-between">
+    <header className="sticky top-0 z-10 mb-10 p-3 border-2 border-gray-800 w-full bg-white flex justify-between">
       <Link href="/">Home</Link>
       {language ? <span>{language}</span> : null}
     </header>

0abfd415a7f5fa65e06be027833668d13e40d09a
Hanif Carroll <hcarroll@leagueapps.com>
2023-04-06 14:00:28 -0300

    Replace initial functionality. Add english featured and good article IDs locally and pull randomly from there.

--- pages/api/getRandomArticleInfo.ts
+++ pages/api/getRandomArticleInfo.ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Article } from '@/utils/types';
-import { getRandomArticleInfo } from '@/utils/utils';
+import { ArticleCategory, getRandomArticleInfo } from '@/utils/utils';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Article[]>
 ) {
   const language = String(req.query.language);
-  return res.status(200).json(await getRandomArticleInfo(language));
+  const type = String(req.query.type) as ArticleCategory;
+  return res.status(200).json(await getRandomArticleInfo({ language, type }));
 }

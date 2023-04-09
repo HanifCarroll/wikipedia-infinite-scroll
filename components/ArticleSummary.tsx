be33c2fdf77e815fb4aa3883f015e2f8b6d09682
Hanif Carroll <hanifcarroll@gmail.com>
2023-04-08 22:30:41 -0300

    Cut summary off at the end of a sentence.

--- components/ArticleSummary.tsx
+++ components/ArticleSummary.tsx
@@ -11,13 +11,17 @@ export function ArticleSummary({
   title,
   url,
 }: ArticleSummaryProps) {
-  summary = summary.length > 400 ? summary.slice(0, 400) + '...' : summary;
+  const summaryCharacterLimit = 400;
+  if (summary.length > summaryCharacterLimit) {
+    const lastPeriodIndex = summary.lastIndexOf('.', summaryCharacterLimit);
+    summary = summary.slice(0, lastPeriodIndex + 1);
+  }
 
   return (
     <a href={url} className="w-11/12 md:w-4/5">
-      <div className="flex flex-col md:flex-row md:items-center h-500 bg-gray-100 border-2 border-gray-500 px-8 py-4 gap-6">
+      <div className="h-500 flex flex-col gap-6 border-2 border-gray-500 bg-gray-100 px-8 py-4 md:flex-row md:items-center">
         <img src={thumbnail} alt={title} />
-        <div className="flex flex-col justify-items-evenly items">
+        <div className="justify-items-evenly items flex flex-col">
           <h2 className="mb-4 text-lg font-medium">{title}</h2>
           <p className="leading-relaxed">{summary}</p>
         </div>
